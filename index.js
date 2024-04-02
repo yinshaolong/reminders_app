7a377bbbb6d19cf50109a5e6e4b4026c3d09a5e9
omgitsjohn <syin18@my.bcit.ca>
2024-04-01 23:19:20 -0700

    made a static express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+//app == advancedServer
+const app = express(); //returns a web server object (store in variable)
+
+//whenev server making request, look for the file in the public folder
+app.use(express.static(path.join(__dirname, 'public'))); //__dirname is the current directory) 
+
+
+//localhost:8081
+app.listen(8081, () => {
+  console.log('Server is running on port 8080');
+});
